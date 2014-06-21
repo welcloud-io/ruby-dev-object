43c4e9b1d6c0e9374b27a3cefb5a5abc11961b35
itarc <itarc@numericable.fr>
2014-06-21 11:13:19 +0200

    adding sign in

--- admin/generators/generator_input.rb
+++ admin/generators/generator_input.rb
@@ -493,11 +493,14 @@ end
 
 TITLE = 'RUBY - Programmation Orientée Objet'    
 SLIDES = [
-{			:Section => [
+{		:Section => [
 			"<h3 style='text-align:center'>INTRODUCTION A RUBY</h3>",
 			"<h3 style='color: red; text-align:center'>CONNECTEZ VOUS SUR :</h3>",
 			"<h4 style='color: blue; font-size: 1.3em; text-align:center'>http://ruby-dev-object.herokuapp.com</h4>",
-			'<div class="code_to_display"> puts "BIENVENUE A LA FORMATION RUBY & ORIENTE OBJET"</div>',
+      "<h3 style='text-align:center'>ET</h3>",
+      "<h3 style='text-align:center'>Entrez Votre Nom</h3>",
+      "<div style='text-align: center'><input id='attendee_name' type='text'></input></div>",
+			'<div class="code_to_display"> puts "CONNEXION REUSSIE, BIENVENUE"</div>',
 			],			
 },
 { :Subtitle => "Les objets", 
