45cc2608f99ab37daafa195759bf3fb4c8259e72
itarc <itarc@numericable.fr>
2014-08-13 15:28:12 +0200

    global.rb replaced by 3 js files

---
+++ public/local.ace_editor_config.js
@@ -0,0 +1,6 @@
+var code_editor = ace.edit(document.getElementById('code_input'));
+Editor.prototype.updateWithText = function (code) { code_editor.setValue(code, -1); };
+Editor.prototype.content = function () { return code_editor.getValue();  };
+code_editor.setTheme('ace/theme/vibrant_ink');
+code_editor.getSession().setMode('ace/mode/ruby');
+code_editor.setFontSize('14px');
--- views/presentation/content.rb
+++ views/presentation/content.rb
@@ -1,7 +1,5 @@
 #encoding: utf-8
 
-require_relative 'globals'
-
 TITLE = 'RUBY - Programmation Orientée Objet'
 
 $slides = []
